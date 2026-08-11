9155918112878270786697527246bd1ef6d48e7a
Admin <pvladan.gimkv@gmail.com>
2026-08-11 10:50:06 +0200

    fix(google-auth): add Web Client ID to constants.dart for Google Sign-In serverClientId

--- chess_app/lib/constants.dart
+++ chess_app/lib/constants.dart
@@ -2,5 +2,5 @@ const String backendUrl = String.fromEnvironment(
   'BACKEND_URL',
   defaultValue: 'http://192.168.0.19:3000',
 );
-const String googleWebClientId = '';
+const String googleWebClientId = '425483567970-jgkipp2dfqmc40kvacn2aginpjnph28s.apps.googleusercontent.com';
 const int kDefaultEngineTargetDepth = 18;
